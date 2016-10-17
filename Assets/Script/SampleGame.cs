864eee0476879bb9bca12192565330f364567c0c
WanJiang018 <hohobbt@gmail.com>
2016-10-17 14:26:53 +0800

    UnityGUI_StarGame

---
+++ Assets/Script/SampleGame.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SampleGame : MonoBehaviour {
+	public GUISkin skin;
+	public GameObject textureObject;
+	
+	private Rect LabelRect;
+	private Rect buttonRect;
+	private List<GameObject> textureList;
+
+	void Start () {
+		LabelRect = new Rect (0,Screen.height-30,100,30);
+		buttonRect = new Rect (0,0,100,45);
+		textureList = new List<GameObject> ();
+	}
+	
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 position=Input.mousePosition;
+			position.x/=Screen.width;
+			position.y/=Screen.height;
+			
+			GameObject star=(GameObject)Instantiate(textureObject,position,Quaternion.identity);
+			
+			textureList.Add (star);
+		}
+	}
+	
+	void OnGUI(){
+		GUI.Label (LabelRect, textureList.Count.ToString (), skin.label);
+		if(GUI.Button (buttonRect,"Clear",skin.button)){
+			foreach(GameObject star in textureList){
+				Destroy(star,0);
+			}
+		}
+		
+		if (GUI.Button (new Rect (0, 55, 100, 45), "END", skin.button)) {
+			foreach(GameObject star in textureList){
+				Destroy(star,0);
+			}
+			textureList.Clear ();
+			
+			SampleMenu menu = gameObject.GetComponent<SampleMenu> ();
+			menu.enabled = true;
+			this.enabled = false;
+		}
+		
+		
+		
+	}
+}
---
+++ Assets/Script/SampleMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleMenu : MonoBehaviour {
+	public GUIStyle btnStyle;
+	private Rect btnRect;
+	private Rect endBtnRect;
+	
+
+	void Start () {
+		btnRect = new Rect (Screen.width*2/5,Screen.height*2/5,256,64);
+		endBtnRect = new Rect (Screen.width*2/5,Screen.height*2/5+74,256,64);
+	}
+	
+
+	void OnGUI () {
+		if (GUI.Button (btnRect, "Start Game", btnStyle)) {
+			SampleGame game=gameObject.GetComponent<SampleGame>();
+			game.enabled=true;
+			this.enabled=false;
+		}
+		
+		if(GUI.Button(endBtnRect,"End Game",btnStyle)){
+			Application.Quit ();
+		}
+	}
+}
